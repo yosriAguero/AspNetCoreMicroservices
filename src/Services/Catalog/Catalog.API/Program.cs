673e946289fb69ff6cfb0a3485235e2423fe1dc1
yosri_jebri <yosri.jebri@outlook.com>
2024-02-09 00:50:40 +0100

    still not working

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,7 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+});
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICatalogContext,CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -13,11 +24,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+   // app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
 }
-
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 app.Run();
--- src/readme.md
+++ src/readme.md
@@ -15,3 +15,15 @@ Abstraction between buisness layer and database context, mock to facilitate test
 
 Builder Mongo 
 Helper class of type of document that contains method like filter and sort
+
+What is the difference between docker compose and docker compose override?
+one is for image definition and other for configuration (inect the app settings variable to override)
+
+What is volume in docker 
+data exists outside container and exists on the host
+
+how to have clear docker ps -a ?
+docker stop, docker rm
+
+docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml up -d
+
